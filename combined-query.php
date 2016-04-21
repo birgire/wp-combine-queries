d471094d8b52627e029e091f80afddad8dbcc629
birgire <birgire@gmail.com>
2016-04-21 04:25:14 -0400

    Version bump

--- combined-query.php
+++ combined-query.php
@@ -7,7 +7,7 @@
  * GitHub Plugin URI: https://github.com/birgire/wp-combined-queries.git
  * Author URI:   https://github.com/birgire
  * License:      MIT
- * Version:      1.0.2
+ * Version:      1.0.3
  */
 
 namespace Birgir\CombinedQuery;
