d34efb3f90ff70428a1cbbdd034fb689b0e74ecf
birgire <birgire@gmail.com>
2016-04-21 04:18:06 -0400

    Minor adjustments on inline docs and  private variables

--- includes/Main.php
+++ includes/Main.php
@@ -14,29 +14,31 @@ class Main
     /**
      * @var string
      */
-    private $orderby        = '';
+    private $orderby;
 
 
     /**
      * @var array
      */
-    private $combined_query = [];
+    private $combined_query;
 
 
     /**
      * @var \Birgir\CombinedQuery\Generator
      */
-    private $generator      = null;
+    private $generator;
 
 
     /**
      * @var \wpdb
      */
-    private $db             = null;
+    private $db;
                               	
 
     /**
      * Init
+     *
+     * @since  1.0.0
      *
      * @param  \Birgir\CombinedQuery\Generator $generator
      * @param  \wpdb $db
@@ -55,6 +57,7 @@ class Main
      * Callback for the 'pre_get_posts' hook
      *
      * @since  1.0.0
+     *
      * @param  \WP_Query $q
      * @return void
      */
@@ -62,6 +65,7 @@ class Main
     {
      	if( $q->get( 'combined_query' ) )
         {
+            // Default arguments
             $defaults = [
                 'union'          => 'UNION',
                 'args'           => [],
@@ -76,109 +80,11 @@ class Main
         }
     }
 
-
     /**
-     * Callback for the 'posts_orderby' hook
+     * Callback for the 'posts_request' filter
      *
      * @since  1.0.0
-     * @param  string $orderby
-     * @return string $orderby
-     */
-    public function posts_orderby( $orderby )
-    {
-        // Only run once:
-     	remove_filter( current_filter(), [ $this, __FUNCTION__ ], PHP_INT_MAX );
-
-        // Store the order by:
-        $this->orderby = $orderby;
-
-        return $orderby;
-    }
-
-
-    /**
-     * Get posts per page
      *
-     * @since  1.0.0
-     * @param  \WP_Query $q
-     * @return int 
-     */
-    public function get_posts_per_page( \WP_Query $q )
-    {
-        return 
-            isset( $q->query_vars['posts_per_page'] ) 
-            ? (int) $q->query_vars['posts_per_page'] 
-            : (int) get_option( 'posts_per_page' );
-    }
-
-
-    /**
-     * Get offset
-     *
-     * @since  1.0.0
-     * @param  \WP_Query $q
-     * @return int
-     */
-    public function get_offset( \WP_Query $q )
-    {
-        return 
-            isset( $this->query_vars['offset'] ) 
-            ? (int) $this->query_vars['offset'] 
-            : 0;
-    }
-
-    /**
-     * Get paged
-     *
-     * @since  1.0.0
-     * @param  \WP_Query $q
-     * @return int 
-     */
-    public function get_paged( \WP_Query $q )
-    {
-        return 
-            isset( $q->query_vars['paged'] ) && 0 < $q->query_vars['paged'] 
-            ? (int) $q->query_vars['paged'] 
-            : 1;
-    }
-
-
-    /**
-     * Get orderby
-     *
-     * @since  1.0.0
-     * @param  \WP_Query $q
-     * @return string 
-     */
-    public function get_orderby( \WP_Query $q )
-    {
-        $orderby = isset( $this->orderby ) ? str_replace( $this->db->posts . '.', '', $this->orderby ) : '';
-        $orderby = apply_filters( 'cq_orderby', $orderby );
-        if( ! empty( $orderby ) )
-            $orderby = ' ORDER BY ' . $orderby;
-        return $orderby;
-    }
-
-
-    /**
-     * Get union
-     *
-     * @since  1.0.0
-     * @return string 
-     */
-    public function get_union()
-    {
-        return 
-            ! in_array( strtoupper( $this->combined_query['union'] ), [ 'UNION', 'UNION ALL' ] ) 
-            ? $this->combined_query['union'] 
-            : 'UNION';
-    }
-
-
-    /**
-     * Get orderby
-     *
-     * @since  1.0.0
      * @param  string	 $request
      * @param  \WP_Query $q
      * @return string 
@@ -200,6 +106,111 @@ class Main
 
         return empty( $generated_request ) ? $request : $generated_request;
     }
+
+
+    /**
+     * Callback for the 'posts_orderby' filter
+     *
+     * @since  1.0.0
+     *
+     * @param  string $orderby
+     * @return string $orderby
+     */
+    public function posts_orderby( $orderby )
+    {
+        // Only run once:
+     	remove_filter( current_filter(), [ $this, __FUNCTION__ ], PHP_INT_MAX );
+
+        // Store the order by:
+        $this->orderby = $orderby;
+
+        return $orderby;
+    }
+
+
+    /**
+     * Get posts per page
+     *
+     * @since  1.0.0
+     *
+     * @param  \WP_Query $q
+     * @return int 
+     */
+    public function get_posts_per_page( \WP_Query $q )
+    {
+        return 
+            isset( $q->query_vars['posts_per_page'] ) 
+            ? (int) $q->query_vars['posts_per_page'] 
+            : (int) get_option( 'posts_per_page' );
+    }
+
+
+    /**
+     * Get offset
+     *
+     * @since  1.0.0
+     *
+     * @param  \WP_Query $q
+     * @return int
+     */
+    public function get_offset( \WP_Query $q )
+    {
+        return 
+            isset( $this->query_vars['offset'] ) 
+            ? (int) $this->query_vars['offset'] 
+            : 0;
+    }
+
+    /**
+     * Get paged
+     *
+     * @since  1.0.0
+     *
+     * @param  \WP_Query $q
+     * @return int 
+     */
+    public function get_paged( \WP_Query $q )
+    {
+        return 
+            ! isset( $q->query_vars['paged'] ) && 0 < $q->query_vars['paged'] 
+            ? (int) $q->query_vars['paged'] 
+            : 1;
+    }
+
+
+    /**
+     * Get orderby
+     *
+     * @since  1.0.0
+     *
+     * @param  \WP_Query $q
+     * @return string 
+     */
+    public function get_orderby( \WP_Query $q )
+    {
+        $orderby = isset( $this->orderby ) ? str_replace( $this->db->posts . '.', '', $this->orderby ) : '';
+        $orderby = apply_filters( 'cq_orderby', $orderby );
+        if( ! empty( $orderby ) )
+            $orderby = ' ORDER BY ' . $orderby;
+        return $orderby;
+    }
+
+
+    /**
+     * Get union
+     *
+     * @since  1.0.0
+     *
+     * @return string 
+     */
+    public function get_union()
+    {
+        return 
+            ! in_array( strtoupper( $this->combined_query['union'] ), [ 'UNION', 'UNION ALL' ] ) 
+            ? $this->combined_query['union'] 
+            : 'UNION';
+    }
+
 
 } // end class
 
