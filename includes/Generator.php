a7e2ceeeea9760f10d69e3ab5f2bcebda1a9b16d
Birgir E <birgire@gmail.com>
2020-09-04 17:11:46 +0000

    Add tests

--- includes/Generator.php
+++ includes/Generator.php
@@ -8,98 +8,92 @@ namespace CombinedQuery;
  * @since 1.0.0
  */
 
-class Generator
-{
-    /**
-     * @var \CombinedQuery\EmptyQuery
-     */
-    private $empty_query;
+class Generator {
+
+	/**
+	 * @var \CombinedQuery\EmptyQuery
+	 */
+	private $empty_query;
 
 
-    /**
-     * Constructor
-     *
-     * @param  \CombinedQuery\EmptyQuery $empty_query
-     * @return void
-     */
-    public function __construct( EmptyQuery $empty_query )
-    {
-        $this->empty_query = $empty_query;
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param  \CombinedQuery\EmptyQuery $empty_query
+	 * @return void
+	 */
+	public function __construct( EmptyQuery $empty_query ) {
+		$this->empty_query = $empty_query;
+	}
 
 
-    /**
-     * @param array $args
-     * @return array $sqls
-     */
-    public function get_sqls( $args = [] )
-    {
-        $sqls = [];
+	/**
+	 * @param array $args
+	 * @return array $sqls
+	 */
+	public function get_sqls( $args = [] ) {
+		$sqls = [];
 
-        $this->empty_query->cq_activate();
+		$this->empty_query->cq_activate();
 
-        // Collect the generated SQL for each sub-query:
-        foreach ( (array) $args as $sub_args )
-        {
-            $this->empty_query->query( $sub_args );
-            $sqls[] = $this->empty_query->cq_get_sql();
-        }
+		// Collect the generated SQL for each sub-query:
+		foreach ( (array) $args as $sub_args ) {
+			$this->empty_query->query( $sub_args );
+			$sqls[] = $this->empty_query->cq_get_sql();
+		}
 
-        $this->empty_query->cq_deactivate();
+		$this->empty_query->cq_deactivate();
 
-        unset( $empty_query );
+		unset( $empty_query );
 
-        return $sqls;
-    }
+		return $sqls;
+	}
 
 
-    /**
-     * Escape % for sprintf
-     *
-     * @since 1.0.2
-     *
-     * @param  string $string
-     * @return string $string
-     */
-    static public function esc_percent( $string )
-    {        
-        return str_replace( '%', '%%', $string );
-    }
+	/**
+	 * Escape % for sprintf
+	 *
+	 * @since 1.0.2
+	 *
+	 * @param  string $string
+	 * @return string $string
+	 */
+	static public function esc_percent( $string ) {
+		return str_replace( '%', '%%', $string );
+	}
 
 
-    /**
-     * Get combined SQL query
-     *
-     * @param  array 	$args
-     * @param  string 	$union
-     * @param  string 	$orderby
-     * @param  int 	$ppp
-     * @param  int 	$paged
-     * @param  int 	$offset
-     * @return string 	$string
-     */
-    public function get_request( $args = [], $union = '', $orderby = '', $ppp = 1, $paged = 1, $offset = 0 )
-    {
-        $request = '';
+	/**
+	 * Get combined SQL query
+	 *
+	 * @param  array    $args
+	 * @param  string   $union
+	 * @param  string   $orderby
+	 * @param  int  $ppp
+	 * @param  int  $paged
+	 * @param  int  $offset
+	 * @return string   $string
+	 */
+	public function get_request( $args = [], $union = '', $orderby = '', $ppp = 1, $paged = 1, $offset = 0 ) {
+		$request = '';
 
-        $sqls = $this->get_sqls( $args );
+		$sqls = $this->get_sqls( $args );
 
-        if ( 0 < count( $sqls ) )
-        {
-            $union = Generator::esc_percent( $union );
-            $sqls  = Generator::esc_percent( $sqls );
+		if ( 0 < count( $sqls ) ) {
+			$union = Generator::esc_percent( $union );
+			$sqls  = Generator::esc_percent( $sqls );
 
-            $unions  = '(' . join( ') ' . $union . ' (', $sqls ) . ' ) ';
+			$unions = '(' . join( ') ' . $union . ' (', $sqls ) . ' ) ';
 
-            $request = sprintf(
-		"SELECT SQL_CALC_FOUND_ROWS * FROM ( {$unions} ) as combined {$orderby} LIMIT %d, %d",
-                $ppp * ( $paged - 1 ) + $offset,
-                $ppp
-            );
-        }
+			$request = sprintf(
+				"SELECT SQL_CALC_FOUND_ROWS * FROM ( {$unions} ) as combined {$orderby} LIMIT %d, %d",
+				$ppp * ( $paged - 1 ) + $offset,
+				$ppp
+			);
+		}
 
-        return $request;
-    }
+		return $request;
+	}
 
 } // end class
 
